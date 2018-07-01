1b18647044ac922963d9afa84152c2360557d100
syntaxsizer <kakuma4never@gmil.com>
2018-07-01 21:06:49 +0300

    create a helper class to abstract requests data parsing

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-from typing import Generator
+import typing
 
 HOST = '127.0.0.1'
 PORT = 3000
@@ -11,8 +11,8 @@ RESPONSE = b"""
 
         <h1>Hello!</h1>""".replace(b"\n", b"\r\n")
 
-def iterate_lines(sock: socket.socket, bufsize: int = 16_384) ->\
-        Generator[bytes, None, bytes]:
+def read_lines(sock: socket.socket, bufsize: int = 16_384) ->\
+        typing.Generator[bytes, None, bytes]:
     """
         Given a socket, read all the individual CRLF-separated lines
         and yield each one until an empty one is found.  Returns the
@@ -36,6 +36,37 @@ def iterate_lines(sock: socket.socket, bufsize: int = 16_384) ->\
                 break
 
 
+class Request(typing.NamedTuple):
+    method: str
+    path: str
+    headers: typing.Mapping[str, str]
+
+    @classmethod
+    def parse(cls, sock: socket.socket) -> "Request":
+        """
+            Read and parse the request from a socket object.
+            Raises: ValueError When the request cannot be parsed.
+        """
+        lines = read_lines(sock)
+        try:
+            request_line = next(lines).decode("ascii")
+        except StopIteration:
+            raise ValueError("Request line missing.")
+
+        try:
+            method, path, _ = request_line.split(" ")
+        except ValueError:
+            raise ValueError(f"Malformed request line {request_line!r}.")
+        headers = {}
+        for line in lines:
+            try:
+                name, _, value = line.decode("ascii").partition(":")
+                headers[name.lower()] = value.lstrip()
+            except ValueError:
+                raise ValueError(f"Malformed header line {line!r}.")
+            return cls(method=method.upper(), path=path, headers=headers)
+
+
 with socket.socket() as ssock:
     ssock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     ssock.bind((HOST, PORT ,))
@@ -45,8 +76,6 @@ with socket.socket() as ssock:
     while True:
         client_sock, client_addr = ssock.accept()
         with client_sock:
-            for request_line in iterate_lines(client_sock):
-                print(request_line)
-
+            request = Request.parse(sock=client_sock)
             client_sock.sendall(RESPONSE)
             print(f"New connection from {client_addr}.")
